e2bbace42bc3d7cad1bc691c4cb2f886999630ba
MeybySumendap <meybysumendap05@gmail.com>
2022-04-01 00:35:58 +0800

    exercise 1

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -17,5 +17,5 @@
 12.jika ya pertahankan angka 4 dan seleksi angka selanjutnya
 13. lakukan perulangan sampai angka 10
 14.setelah angka genap di simpan selanjutnya ditambahkan
-15.hasilnya akan seperti berikut 2 + 4 + 6 + 8 + 10
+15.hasilnya akan seperti berikut 2 + 4 + 6 + 8  = 30
 */
